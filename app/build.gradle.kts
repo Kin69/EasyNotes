5b1eb223d1137266e6326af1ce64b1973676547f
Kin69 <nikooravec1@proton.me>
2024-05-24 19:27:36 +0200

    Update version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kin.easynotes"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         vectorDrawables {
             useSupportLibrary = true
         }
