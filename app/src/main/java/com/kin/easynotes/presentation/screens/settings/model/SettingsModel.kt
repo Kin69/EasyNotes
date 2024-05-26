a0f30154a0eb46e5a54fc1aa50ec9ff54f74cfa7
Kin69 <Vexzure@proton.me>
2024-05-26 14:48:39 +0200

    Get app version automatically in setting

--- app/src/main/java/com/kin/easynotes/presentation/screens/settings/model/SettingsModel.kt
+++ app/src/main/java/com/kin/easynotes/presentation/screens/settings/model/SettingsModel.kt
@@ -3,12 +3,12 @@ package com.kin.easynotes.presentation.screens.settings.model
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
+import com.kin.easynotes.BuildConfig
 import com.kin.easynotes.domain.usecase.NoteViewModel
 import com.kin.easynotes.domain.usecase.Preferences
 
 class SettingsViewModel : NoteViewModel() {
-    var version: String = "0.0.1"
+    var version = BuildConfig.VERSION_NAME
     var dynamicTheme : Boolean by mutableStateOf(getValue("DYNAMIC_THEME"))
     var darkTheme : Boolean by mutableStateOf(getValue("DARK_THEME"))
     var amoledTheme  : Boolean by mutableStateOf(getValue("AMOLED_THEME"))
