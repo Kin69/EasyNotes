c9587621f875898581e56b308d7c8b1224b9d8af
Kin69 <Vexzure@proton.me>
2024-07-02 14:25:12 +0200

    Update App version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kin.easynotes"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.2.5"
         vectorDrawables {
             useSupportLibrary = true
         }
