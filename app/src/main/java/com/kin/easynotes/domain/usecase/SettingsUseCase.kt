aebd3df6d37b79ea417669e8536b457d0cbfae9b
Kin69 <Vexzure@proton.me>
2024-08-09 03:53:49 +0200

    Add handlers for preferences Fix: Crashing after updating

--- app/src/main/java/com/kin/easynotes/domain/usecase/SettingsUseCase.kt
+++ app/src/main/java/com/kin/easynotes/domain/usecase/SettingsUseCase.kt
@@ -1,50 +1,61 @@
 package com.kin.easynotes.domain.usecase
 
-import android.content.Context
+import com.kin.easynotes.core.constant.ConnectionConst
 import com.kin.easynotes.data.repository.SettingsRepositoryImpl
 import com.kin.easynotes.domain.model.Settings
-import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.CoroutineScope
 import javax.inject.Inject
 
 class SettingsUseCase @Inject constructor(
     private val settingsRepository: SettingsRepositoryImpl,
-    private val coroutineScope: CoroutineScope,
-    @ApplicationContext private val context: Context
-) {
-    suspend fun loadSettingsFromRepository(): Settings {
-        val settingsClass = Settings::class.java
-        val defaultSettings = Settings()
-        val settings = settingsClass.getDeclaredConstructor().newInstance()
+    ) {
 
-        settingsClass.declaredFields.forEach { field ->
-            field.isAccessible = true
-            val settingName = field.name
-            val defaultValue = field.get(defaultSettings)
-            val settingValue = when (field.type) {
+    suspend fun loadSettingsFromRepository(): Settings {
+        return Settings().apply {
+            Settings::class.java.declaredFields.forEach { field ->
+                field.isAccessible = true
+                val settingName = field.name
+                val defaultValue = field.get(this)
+                val settingValue = getSettingValue(field.type, settingName, defaultValue)
+                field.set(this, settingValue)
+            }
+        }
+    }
+
+    private suspend fun getSettingValue(fieldType: Class<*>, settingName: String, defaultValue: Any?): Any? {
+        return try {
+            when (fieldType) {
                 Boolean::class.java -> settingsRepository.getBoolean(settingName) ?: defaultValue
                 String::class.java -> settingsRepository.getString(settingName) ?: defaultValue
                 Int::class.java -> settingsRepository.getInt(settingName) ?: defaultValue
-                else -> throw IllegalArgumentException("Unsupported setting type: ${field.type}")
+                else -> throw IllegalArgumentException("Unsupported setting type: $fieldType")
             }
-            field.set(settings, settingValue)
+        } catch (e: ClassCastException) {
+            handleCorruptedPreference(settingName, e)
+            defaultValue
         }
-        return settings
     }
 
-   suspend fun saveSettingsToRepository(settings: Settings) {
-        val settingsClass = Settings::class.java
+    private fun handleCorruptedPreference(settingName: String, e: ClassCastException) {
+        println("Corrupted preference. Contact support: ${ConnectionConst.SUPPORT_MAIL}")
+        println("Invalid Key: $settingName")
+        println(e.stackTraceToString())
+    }
 
-        settingsClass.declaredFields.forEach { field ->
+    suspend fun saveSettingsToRepository(settings: Settings) {
+        Settings::class.java.declaredFields.forEach { field ->
             field.isAccessible = true
             val settingName = field.name
             val settingValue = field.get(settings)
-            when (settingValue) {
-                is Boolean -> settingsRepository.putBoolean(settingName, settingValue)
-                is String -> settingsRepository.putString(settingName, settingValue)
-                is Int -> settingsRepository.putInt(settingName, settingValue)
-                else -> throw IllegalArgumentException("Unsupported setting type: ${settingValue?.javaClass}")
-            }
+            saveSettingValue(settingName, settingValue)
+        }
+    }
+
+    private suspend fun saveSettingValue(settingName: String, settingValue: Any?) {
+        when (settingValue) {
+            is Boolean -> settingsRepository.putBoolean(settingName, settingValue)
+            is String -> settingsRepository.putString(settingName, settingValue)
+            is Int -> settingsRepository.putInt(settingName, settingValue)
+            else -> throw IllegalArgumentException("Unsupported setting type: ${settingValue?.javaClass}")
         }
     }
 }
